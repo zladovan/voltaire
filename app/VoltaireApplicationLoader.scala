2c9df1119a8ba09ad6071638d4df152e5b30d322
zladovan <cabala.jan@gmail.com>
2015-08-09 11:19:39 +0200

    Fixed missing ExecutionContext import

--- app/VoltaireApplicationLoader.scala
+++ app/VoltaireApplicationLoader.scala
@@ -6,6 +6,8 @@ import play.api.routing.Router
 import router.Routes
 import services.ArticlesUpdateModule
 
+import scala.concurrent.ExecutionContext
+
 /**
  * Created with IntelliJ IDEA.
  * User: zladovan
@@ -21,6 +23,7 @@ class VoltaireApplicationLoader extends ApplicationLoader {
 
 trait Components extends BuiltInComponents with ApplicationComponents with ActorsModule with ArticlesUpdateModule {
   import com.softwaremill.macwire.MacwireMacros._
+  import ExecutionContext.Implicits.global
 
   lazy val assets: Assets = wire[Assets]
   lazy val router: Router = wire[Routes] withPrefix "/"
